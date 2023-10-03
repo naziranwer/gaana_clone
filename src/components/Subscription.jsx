b2252fe894fff5e6817467644b5f9c7839899eac
Mohd Nazir Anwer <anwernazir674@gmail.com>
2023-10-03 13:23:34 +0530

    update

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -1,150 +1,8 @@
-// import React, { useState } from "react";
-
-// import subscriptionImage from "./assets/subscription.png";
-// import { Link, Outlet, useNavigate } from "react-router-dom";
-
-// const Subscription = () => {
-//   // State to track selected subscription duration
-//   const [selectedDuration, setSelectedDuration] = useState(null);
-//   const isLoggedIn = JSON.parse(localStorage.getItem("user"));
-//   const navigate = useNavigate();
-
-//   console.log("isLo", isLoggedIn);
-
-//   const handleDurationChange = (duration) => {
-//     setSelectedDuration(duration);
-//   };
-
-//   const containerStyle = {
-//     // height: "50vh",
-//     display: "flex",
-//     flexDirection: "column",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   };
-
-//   const imageStyle = {
-//     height: "35vh",
-//     width: "105%",
-//     padding: "2px",
-//   };
-
-//   const checkboxContainerStyle = {
-//     display: "flex",
-//     flexDirection: "row",
-//     justifyContent: "space-between",
-//     width: "100%",
-//     marginTop: "45px",
-//   };
-
-//   const checkboxBoxStyle = {
-//     border: "5px solid white",
-//     color: "orange",
-//     padding: "10px",
-//     marginBottom: "10px",
-//     width: "30%",
-//     borderRadius: "15px", // Add border radius
-//     display: "flex",
-//     alignItems: "center", // Center text vertically
-//     justifyContent: "center", // Center text horizontally
-//   };
-
-//   const checkboxLabelStyle = {
-//     display: "flex",
-//     alignItems: "center",
-//     justifyContent: "center",
-//     width: "100%",
-//   };
-
-//   const handleSubsc = () => {
-//     // localStorage.setItem(`${isLoggedIn.data.email}`, selectedDuration);
-//     navigate("/payment", { state: selectedDuration });
-//   };
-
-//   const storedDuration = localStorage.getItem(`${isLoggedIn.data.email}`);
-
-//   return (
-//     <div style={containerStyle}>
-//       <img src={subscriptionImage} alt="Image Alt Text" style={imageStyle} />
-//       <div className="plan-option" style={checkboxContainerStyle}>
-//         {/* Checkbox for 1 month */}
-//         <div style={checkboxBoxStyle}>
-//           <label style={checkboxLabelStyle}>
-//             <input
-//               type="checkbox"
-//               name="duration"
-//               value="1month"
-//               checked={selectedDuration === "1month"}
-//               onChange={() => handleDurationChange("1month")}
-//               disabled={
-//                 storedDuration === "1month" ||
-//                 storedDuration === "6months" ||
-//                 storedDuration === "1Year"
-//               }
-//             />
-//             1 Month Gaana Plus Gaana Plus &#8377;99
-//           </label>
-//         </div>
-//         {/* Checkbox for 6 months */}
-//         <div style={checkboxBoxStyle}>
-//           <label style={checkboxLabelStyle}>
-//             <input
-//               type="checkbox"
-//               name="duration"
-//               value="6months"
-//               checked={selectedDuration === "6months"}
-//               onChange={() => handleDurationChange("6months")}
-//               disabled={
-//                 storedDuration === "6months" || storedDuration === "1Year"
-//               }
-//             />
-//             6 Months Gaana Plus Gaana Plus &#8377;249
-//           </label>
-//         </div>
-//         {/* Checkbox for 1 Year */}
-//         <div style={checkboxBoxStyle}>
-//           <label style={checkboxLabelStyle}>
-//             <input
-//               type="checkbox"
-//               name="duration"
-//               value="1Year"
-//               checked={selectedDuration === "1Year"}
-//               onChange={() => handleDurationChange("1Year")}
-//               disabled={storedDuration === "1Year"}
-//             />
-//             1 Year Gaana Plus Gaana Plus &#8377;399
-//           </label>
-//         </div>
-//       </div>
-//       <div style={{ textAlign: "center" }}></div>
-//       {/* <Link to="/payment"> */}
-//       <button
-//         style={{
-//           backgroundColor: "#E72C30",
-//           borderRadius: "20px",
-//           padding: "40x 40px",
-//           margin: "40px 6px 6px",
-//           width: "120px",
-//           height: "30px",
-//           cursor: "pointer",
-//         }}
-//         onClick={handleSubsc}
-//         // className={theme.palette.mode === "dark" ? "gplusdark" : "gplus mr60"}
-//       >
-//         SUBSCRIBE NOW
-//       </button>
-//       {/* </Link> */}
-//       {/* <Outlet /> */}
-//     </div>
-//   );
-// };
-
-// export default Subscription;
-
 import React, { useState } from "react";
 import subscriptionImage from "./assets/subscription.png";
 import { Link, useNavigate } from "react-router-dom";
 import { Button, Checkbox, FormControlLabel, Box } from "@mui/material";
+import { toast } from "react-toastify";
 
 const Subscription = () => {
   const [selectedDuration, setSelectedDuration] = useState(null);
@@ -156,7 +14,9 @@ const Subscription = () => {
   };
 
   const handleSubsc = () => {
-    navigate("/payment", { state: selectedDuration });
+    if (selectedDuration !== null) {
+      navigate("/payment", { state: selectedDuration });
+    } else toast.error("Please select any checkbox");
   };
 
   const storedDuration = localStorage.getItem(`${isLoggedIn?.data?.email}`);
